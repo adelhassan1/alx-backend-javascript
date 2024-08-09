dfa8088b6c34be648ecad13e96a1dd0c23d49628
adelhassan1 <tomasander12@gmail.com>
2024-08-10 02:31:31 +0300

    promise

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,10 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const userPromise = await createUser();
-  const photoPromise = await uploadPhoto();
-
   try {
+    const userPromise = await createUser();
+    const photoPromise = await uploadPhoto();
+
     return ({ photo: photoPromise, user: userPromise });
   } catch (e) {
     return ({ photo: null, user: null });
